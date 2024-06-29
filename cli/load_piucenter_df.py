fef84a9de73e7056d7747e02387abaf8c0ea5bcc
Max Shen <maxwshen@gmail.com>
2024-06-29 10:03:40 -0700

    update

--- cli/load_piucenter_df.py
+++ cli/load_piucenter_df.py
@@ -1,17 +1,24 @@
 import argparse
+import os
 from hackerargs import args
+from loguru import logger
 
 from piu_annotate.formats.piucenterdf import PiuCenterDataFrame
 from piu_annotate.formats.chart import ChartStruct
 from piu_annotate.formats.jsplot import ChartJsStruct
 
 def main():
-    pc_df = PiuCenterDataFrame(args['file'])
+    file = args['file']
+    logger.info(f'Loaded {file} ...')
+
+    pc_df = PiuCenterDataFrame(file)
     cs = ChartStruct.from_piucenterdataframe(pc_df)
 
     cjss = ChartJsStruct.from_chartstruct(cs)
 
-    import code; code.interact(local=dict(globals(), **locals()))
+    out_file = os.path.join('/home/maxwshen/piu-annotate/output/chart-json', 'conflict-d24.json')
+    cjss.to_json(out_file)
+    logger.info(f'Wrote to {out_file}.')
     return
 
 
--- piu_annotate/formats/jsplot.py
+++ piu_annotate/formats/jsplot.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import os
-import pickle
-import gzip
+from pathlib import Path
+import json
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
@@ -18,8 +18,10 @@ class ArrowArt:
     def validate(self):
         assert self.arrow_pos in list(range(0, 10))
         assert self.time > 0
-        assert self.limb in ['l', 'r', 'e', 'h']
+        assert self.limb in set(list('lreh?'))
 
+    def to_tuple(self) -> list:
+        return (self.arrow_pos, self.time, self.limb)
 
 @dataclass
 class HoldArt:
@@ -31,7 +33,10 @@ class HoldArt:
     def validate(self):
         assert self.arrow_pos in list(range(0, 10))
         assert self.start_time > 0 and self.end_time > 0
-        assert self.limb in ['l', 'r', 'e', 'h']
+        assert self.limb in set(list('lreh?'))
+
+    def to_tuple(self) -> list:
+        return (self.arrow_pos, self.start_time, self.end_time, self.limb)
 
 
 class ChartJsStruct:
@@ -44,12 +49,27 @@ class ChartJsStruct:
         """
         self.arrow_arts = arrow_arts
         self.hold_arts = hold_arts
+        self.json_struct = self.get_json_struct()
         pass
 
     @staticmethod
     def from_chartstruct(cs: ChartStruct):
         return ChartJsStruct(*cs.get_arrow_hold_arts())
     
-    def to_file(self):
-        """ Save to file: pkl.gz, for """
+    def get_json_struct(self):
+        """ Get representation in terms of lists and basic objects.
+            Returns a tuple, containing:
+            - list of arrow art tuples
+            - list of hold art tuples
+        """
+        return (
+            [art.to_tuple() for art in self.arrow_arts],
+            [art.to_tuple() for art in self.hold_arts],
+        )
+
+    def to_json(self, filename: str):
+        """ Save to json file """
+        Path(os.path.dirname(filename)).mkdir(parents = True, exist_ok = True)
+        with open(filename, 'w') as f:
+            json.dump(self.json_struct, f)
         return
