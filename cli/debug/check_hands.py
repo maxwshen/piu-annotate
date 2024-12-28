20e98b66857f6c4600405621b4430b17db3e0e68
Max Shen <maxwshen@gmail.com>
2024-12-28 13:44:59 -0500

    update check hands

--- cli/debug/check_hands.py
+++ cli/debug/check_hands.py
@@ -8,22 +8,36 @@ import sys
 
 from piu_annotate.formats.chart import ChartStruct
 from piu_annotate import utils
-from piu_annotate.segment.skills import check_hands
+from piu_annotate.segment.skills import hands
 
 
-def segment_single_chart(csv: str):
-    logger.info(f'Running on {csv=}')
-    cs = ChartStruct.from_file(csv)
-    check_hands(cs)
-    return
-
+ok_hands = [
+    'Ugly_Dee_-_Banya_Production_D18_ARCADE',
+    'Ugly_Dee_-_Banya_Production_D17_ARCADE',
+    'Come_to_Me_-_Banya_S17_INFOBAR_TITLE_ARCADE',
+    'ESCAPE_-_D_AAN_D26_ARCADE',
+    'Chimera_-_YAHPP_S23_ARCADE',
+    'Chimera_-_YAHPP_D26_ARCADE',
+    'Come_to_Me_-_Banya_S13_ARCADE',
+    'Naissance_2_-_BanYa_D16_ARCADE',
+    'Achluoias_-_D_AAN_D26_ARCADE',
+    'Jump_-_BanYa_S16_ARCADE',
+    'Gun_Rock_-_Banya_Production_S20_ARCADE',
+    'Uh-Heung_-_DKZ_S22_ARCADE',
+    'Love_is_a_Danger_Zone_2_-_FULL_SONG_-_-_Yahpp_S20_FULLSONG',
+    'Love_is_a_Danger_Zone_2_-_FULL_SONG_-_-_Yahpp_D21_FULLSONG',
+    'Love_is_a_Danger_Zone_2_Try_To_B.P.M_-_BanYa_D23_INFOBAR_TITLE_REMIX',
+    'Love_is_a_Danger_Zone_2_Try_To_B.P.M_-_BanYa_S21_REMIX',
+    'Hi-Bi_-_BanYa_D21_ARCADE',
+    'Fire_Noodle_Challenge_-_Memme_S23_REMIX',
+    'Slam_-_Novasonic_S18_INFOBAR_TITLE_ARCADE',
+    'Bee_-_BanYa_D23_ARCADE',
+    'Bee_-_BanYa_S17_INFOBAR_TITLE_ARCADE',
+    'Another_Truth_-_Novasonic_D19_ARCADE',
+]
 
 def main():
-    if 'csv' in args and args['csv'] is not None:
-        # run single
-        logger.info(f'Running single ...')
-        segment_single_chart(args['csv'])
-        return
+
 
     # run on folder
     cs_folder = args['chart_struct_csv_folder']
@@ -31,11 +45,20 @@ def main():
     chartstruct_files = [fn for fn in os.listdir(cs_folder) if fn.endswith('.csv')]
     logger.info(f'Found {len(chartstruct_files)} ChartStruct CSVs ...')
 
-    from collections import defaultdict
     for cs_file in tqdm(chartstruct_files):
         inp_fn = os.path.join(cs_folder, cs_file)
         cs = ChartStruct.from_file(inp_fn)
-        check_hands(cs)
+
+        hands(cs)
+        has_hands = any(cs.df['__hands'])
+
+        if has_hands:
+            shortname = cs.metadata["shortname"]
+            if shortname not in ok_hands:
+                logger.debug(f'Found hands')
+                logger.debug(f'{cs.metadata["shortname"]}')
+                print(cs.df[cs.df['__hands']])
+                import code; code.interact(local=dict(globals(), **locals()))
 
     logger.success('done')
     return
@@ -50,7 +73,7 @@ if __name__ == '__main__':
     """)
     parser.add_argument(
         '--chart_struct_csv_folder', 
-        default = '/home/maxwshen/piu-annotate/artifacts/chartstructs/092424/lgbm-110424/',
+        default = '/home/maxwshen/piu-annotate/artifacts/chartstructs/120524/lgbm-120524/',
         # default = '/home/maxwshen/piu-annotate/artifacts/chartstructs/092424',
     )
     parser.add_argument(
